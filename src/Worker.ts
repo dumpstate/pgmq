93bc53dd9f9b63594cfdce9feb3211b0938a59e6
Albert Sadowski <asadowski@mailbox.org>
2023-05-07 06:53:23 +0100

    add logging

--- src/Worker.ts
+++ src/Worker.ts
@@ -1,4 +1,5 @@
 import { DBAction, pure } from "@dumpstate/dbaction/lib/PG"
+import { Logger, newLogger } from "@dumpstate/bongojs/lib/logger"
 import { Bongo, Document } from "@dumpstate/bongojs"
 import { forever } from "async"
 import { PoolClient } from "pg"
@@ -6,14 +7,27 @@ import { Queue } from "./Queue"
 import { QueueType } from "./model"
 
 export abstract class Worker {
-	public constructor(private readonly queue: Queue) {}
+	private readonly logger: Logger
+
+	public constructor(private readonly queue: Queue, logger?: Logger) {
+		if (logger) {
+			this.logger = logger
+		} else {
+			this.logger = newLogger({ name: "pgmq", level: "info" })
+		}
+	}
 
 	abstract process(task: Document<QueueType>): Promise<any>
 
 	public run(bongo: Bongo) {
+		this.logger.info(`Starting worker loop: ${this.queue.name}`)
+		let count = 0
+
 		forever(
-			async () =>
-				this.queue
+			async () => {
+				this.logger.info(`Worker loop ${count}`)
+				count += 1
+				return this.queue
 					.dequeue()
 					.flatMap((task) => {
 						if (task === null) {
@@ -22,10 +36,19 @@ export abstract class Worker {
 
 						return new DBAction((conn: PoolClient) =>
 							this.process(task)
-								.then(() =>
-									this.queue.moveToDone(task).action(conn)
-								)
+								.then(() => {
+									this.logger.info(
+										`Moving task ${task.id} to DONE`
+									)
+									return this.queue
+										.moveToDone(task)
+										.action(conn)
+								})
 								.catch((err) => {
+									this.logger.error(
+										`err when processing task ${task.id}`,
+										err
+									)
 									if (task.attempts$ > 3) {
 										return this.queue
 											.moveToDlq(task, err)
@@ -41,13 +64,15 @@ export abstract class Worker {
 					.transact(bongo.tr)
 					.then((res) => {
 						if (res === null) {
+							this.logger.info("Queue is empty, waiting")
 							return new Promise((resolve) =>
-								setTimeout(resolve, 200)
+								setTimeout(resolve, 1000)
 							)
 						} else {
 							return null
 						}
-					}),
+					})
+			},
 			(err) => {
 				console.error(err)
 				process.exit(1)
